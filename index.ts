91b559061c0f6e54f6dbcb608640356a5637e12f
Hyeon Kim <simnalamburt@gmail.com>
2018-12-18 22:01:41 +0900

    전역변수로 깔아서 쓰고싶은 상황

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
 
-document.body.innerHTML = `
-  이상한 방법을 추천드려서 죄송합니다
-  <br>
-  clamp(3.14, 0.0, 1.0) = ${THREE.Math.clamp(3.14, 0.0, 1.0)}
-`
+document.body.insertAdjacentHTML('beforeend', `
+  <div>
+    이상한 방법을 추천드려서 죄송합니다
+    <br>
+    clamp(3.14, 0.0, 1.0) = ${THREE.Math.clamp(3.14, 0.0, 1.0)}
+  </div>
+`)
