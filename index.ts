0a863f23daa5e2c4d9805c0b71ca5203fa8e80b3
Hyeon Kim <simnalamburt@gmail.com>
2020-09-21 05:11:46 +0900

    Update three.js

--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ document.body.insertAdjacentHTML('beforeend', `
   <div>
     이상한 방법을 추천드려서 죄송합니다
     <br>
-    clamp(3.14, 0.0, 1.0) = ${THREE.Math.clamp(3.14, 0.0, 1.0)}
+    clamp(3.14, 0.0, 1.0) = ${THREE.MathUtils.clamp(3.14, 0.0, 1.0)}
   </div>
 `)
