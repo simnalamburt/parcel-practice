27642d3ea7ff1650190347a5d36714d3f6ae8c4f
Hyeon Kim <simnalamburt@gmail.com>
2020-09-21 05:22:03 +0900

    Update codes

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import * as THREE from 'three'
 
-window.THREE = THREE
+globalThis.THREE = THREE
 
 document.body.insertAdjacentHTML(
   'beforeend',
-  `
-  <div>
-    이상한 방법을 추천드려서 죄송합니다
+  `<div>
+    Calling three.js in index.ts
     <br>
     clamp(3.14, 0.0, 1.0) = ${THREE.MathUtils.clamp(3.14, 0.0, 1.0)}
-  </div>
-`
+  </div>`
 )
