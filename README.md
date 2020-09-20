604f74f7eab701b871bbed97f06f262a5ee28eed
Hyeon Kim <simnalamburt@gmail.com>
2020-09-21 05:15:35 +0900

    Adopt Prettier

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ directory for production-ready build results.
 
 ```bash
 yarn test
+yarn fmt
 ```
--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@ import * as THREE from 'three'
 
 window.THREE = THREE
 
-document.body.insertAdjacentHTML('beforeend', `
+document.body.insertAdjacentHTML(
+  'beforeend',
+  `
   <div>
     이상한 방법을 추천드려서 죄송합니다
     <br>
     clamp(3.14, 0.0, 1.0) = ${THREE.MathUtils.clamp(3.14, 0.0, 1.0)}
   </div>
-`)
+`
+)
