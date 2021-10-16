bb5b551d0121332b25ddb10a71f50c69d0e52664
Hyeon Kim <simnalamburt@gmail.com>
2021-10-16 23:34:03 +0900

    Use <script type=module>

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ globalThis.THREE = THREE
 
 document.body.insertAdjacentHTML(
   'beforeend',
-  `<div>
+  `<div style="background-color: #e0ffe5">
     Calling three.js in index.ts
     <br>
     clamp(3.14, 0.0, 1.0) = ${THREE.MathUtils.clamp(3.14, 0.0, 1.0)}
